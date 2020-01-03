091d3b84d3c0fef40a8c4277660c81404a63f9a7
Alghifari Muhammad Dzar <alghifari.m.d@gmail.com>
2020-01-03 19:10:04 +0300

    Tugas 19 JS
    Tugas ini untuk Niomic

---
+++ Tugas19.js
@@ -0,0 +1,21 @@
+function tugas19() {
+var str1 = "Semangat...";
+var str2 = "*";
+
+console.log();
+console.log("1.");
+console.log(str1.repeat(5));
+console.log();
+console.log("2.");
+console.log(str2.repeat(5));
+console.log(str2.repeat(30));
+console.log(str2.repeat(45));
+console.log(str2.repeat(60));
+console.log(str2.repeat(80));
+console.log(str2.repeat(60));
+console.log(str2.repeat(45));
+console.log(str2.repeat(30));
+console.log(str2.repeat(5));
+
+}
+tugas19();
